f6fe82cae16cb532b56c283b73a6865129141b68
trzynasty-0 <goniwiechamaja123@gmail.com>
2025-11-03 22:27:01 +0100

    poprawienie wywietlania polubień i dodanie działalnoci guzika 'usuń'

--- app/src/main/java/com/example/zadankozczerwca/MainActivity.java
+++ app/src/main/java/com/example/zadankozczerwca/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.zadankozczerwca;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button polub, zapisz, usun;
+    private TextView polubienia;
+    private int iloscPolubien = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +27,47 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        polub = findViewById(R.id.button);
+        zapisz = findViewById(R.id.button3);
+        polubienia = findViewById(R.id.textView2);
+        usun = findViewById(R.id.button2);
+
+        if(savedInstanceState != null){
+            iloscPolubien =  savedInstanceState.getInt("wartoscZapisanychPolubien");
+            polubienia.setText(iloscPolubien + " polubień");
+
+        }
+
+        usun.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(iloscPolubien > 0){
+                            iloscPolubien--;
+                            polubienia.setText(iloscPolubien + " polubień");
+                        }
+
+                    }
+                }
+        );
+
+        polub.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        iloscPolubien++;
+                        polubienia.setText(iloscPolubien + " polubień");
+                    }
+                }
+        );
+
     }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("wartoscZapisanychPolubien", iloscPolubien);
+    }
+
+
 }
